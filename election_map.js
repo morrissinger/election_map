9861cc4d1b8a2d048f2443c3a01dd69185785703
Morris Singer <morrissinger@gmail.com>
2012-09-19 16:46:50 -0400

    backported for earlier jQuery versions for Drupal6 compliance.

--- election_map.js
+++ election_map.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
+	$("body").prepend("<div class=\"popover right\"></div>").show();
 	$(".electionmap area.region").bind("mouseover", function(event){
 		bottom = event.pageY;
 		left = event.pageX;
-		$.get('?q=election_map/request/'+$(this).data('tid'), function(data){
-			$("body").prepend(data).children(".popover").css("top", (bottom-($(".popover").height()/2))+"px").css("left", (left+5)+"px").fadeIn();
+		$.get('?q=election_map/request/'+this.id, function(data){
+			$(".popover").prepend(data).css("top", (bottom-($(".popover").height()/2))+"px").css("left", (left+5)+"px").fadeIn("fast");
 		});
 	}).bind("mouseout", function(event){
-		$(".popover").detach()
+		$(".popover").empty().hide()
 	});	
 });
