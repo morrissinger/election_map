83ab5274ae6c8e1b80f9b3fe0526faf7a262c99a
Morris Singer <morrissinger@gmail.com>
2012-09-20 11:43:17 -0400

    added loading modal for ahah.

--- election_map_ahah.js
+++ election_map_ahah.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
+	num_els = $(".electionmap area.region").length;
+	i = 1
+	$(".electionmap").prepend("<div class=\"modal\"></div>")
+						.children(".modal").prepend("<div class=\"loading\"><h2>Please wait</h2><div class=\"content\"><p>The map data is loading. Once it is loaded, you may hover over a region to view election data.</div><div class=\"throbber\">Loading data...</div>").children(".loading").hide().slideDown();
+
 	$(".electionmap area.region").each(function(){
 		$.get('?q=election_map/request/'+this.id, function(data){
 			$("body").prepend(data).show();
+			i++;
+			if(i >= num_els)
+				{
+				$(".modal").fadeOut();
+				}
 		});
 
 	});
