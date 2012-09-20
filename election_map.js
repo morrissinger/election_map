ae99cd41f989a9dcfe26e87d10de7ee6c587ba00
Morris Singer <morrissinger@gmail.com>
2012-09-20 12:30:21 -0400

    optimized server-side code.

--- election_map.js
+++ election_map.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 		bottom = event.pageY;
 		left = event.pageX;
 		$(".popover").css("top", (bottom-($(".popover").height()/2))+"px").css("left", (left+5)+"px").fadeIn("fast");
-		$.get('?q=election_map/request/'+this.id, function(data){
+		$.get('?q=election_map/request/'+JSON.stringify(this.id), function(data){
 			$("body").prepend(data).children(".popover").show().css("top", (bottom-($(".popover").height()/2))+"px").css("left", (left+5)+"px").children(".throbber").hide();
 		});
 	}).bind("mouseout", function(event){
--- election_map_ahah.js
+++ election_map_ahah.js
@@ -1,20 +1,20 @@
 $(document).ready(function(){
-	num_els = $(".electionmap area.region").length;
-	i = 1
-	$(".electionmap").prepend("<div class=\"modal\"></div>")
+	tids = [];
+	
+	$("body").prepend("<div class=\"modal\"></div>")
 						.children(".modal").prepend("<div class=\"loading\"><h2>Please wait</h2><div class=\"content\"><p>The map is loading data. Once it is loaded, simply hover over a colored county to view SEIU 1021's voting recommendations.</div><div class=\"throbber\">Loading data...</div>").children(".loading").hide().slideDown();
 
 	$(".electionmap area.region").each(function(){
-		$.get('?q=election_map/request/'+this.id, function(data){
-			$("body").prepend(data).show();
-			i++;
-			if(i >= num_els)
-				{
-				$(".modal").fadeOut();
-				}
-		});
-
+		if (this.id)
+		{
+		tids.push(this.id);
+		}
 	});
+	
+	$.get('?q=election_map/request/'+JSON.stringify(tids), function(data){
+		$("body").prepend(data).show().children(".modal").fadeOut();
+	});
+	
 	
 	$(".electionmap area.region").bind("mouseover", function(event){
 		bottom = event.pageY;
